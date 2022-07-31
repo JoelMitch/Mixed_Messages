d8f8f3d53b72ebeb12dcff43ce5299b0e9aa7957
JoelMitch <107200469+JoelMitch@users.noreply.github.com>
2022-07-31 11:54:03 +0100

    Added message components

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,3 +1,13 @@
 /* Idea: A random meal generator that will randomly select three components that make up a meal.
 The different components will consist of a type of protein, a side, and a type of vegetable. 
 These will then be logged in a format so the user can see the meal that has been chosen for them.*/
+
+const mealComponents = {
+    protein: ['sausages', 'burger', 'steak', 'bacon', 'cod', 'roast chicken', 'chicken wings', 
+    'chicken kiev', 'bbq ribs', 'salmon', 'quorn burger', 'egg'],
+    vegetable: ['carrots', 'peas', 'brocolli', 'sweetcorn', 'brussel sprouts', 'pepper', 'pumpkin', 
+    'asparagus', 'cauliflower', 'onion', ],
+    side: ['garlic bread', 'fries', 'salad', 'sweet potato fries', 'pasta', 'spinach', 'coleslaw', 
+    'jacket potato', 'baked beans'],
+}
+
