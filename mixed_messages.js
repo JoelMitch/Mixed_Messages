2040921ccce83e9755f43fd19be9549e81cb76e4
JoelMitch <107200469+JoelMitch@users.noreply.github.com>
2022-07-31 16:05:31 +0100

    added functionality to randomly select components

--- mixed_messages.js
+++ mixed_messages.js
@@ -3,11 +3,38 @@ The different components will consist of a type of protein, a side, and a type o
 These will then be logged in a format so the user can see the meal that has been chosen for them.*/
 
 const mealComponents = {
-    protein: ['sausages', 'burger', 'steak', 'bacon', 'cod', 'roast chicken', 'chicken wings', 
-    'chicken kiev', 'bbq ribs', 'salmon', 'quorn burger', 'egg'],
-    vegetable: ['carrots', 'peas', 'brocolli', 'sweetcorn', 'brussel sprouts', 'pepper', 'pumpkin', 
-    'asparagus', 'cauliflower', 'onion', ],
+    protein: ['sausages', 'burgers', 'steak', 'bacon', 'cod', 'roast chicken', 'chicken wings', 
+    'chicken kievs', 'bbq ribs', 'salmon', 'quorn burgers', 'eggs'],
+    vegetable: ['carrots', 'peas', 'brocolli', 'sweetcorn', 'brussel sprouts', 'peppers', 'roasted pumpkin', 
+    'asparagus', 'cauliflower', 'onion'],
     side: ['garlic bread', 'fries', 'salad', 'sweet potato fries', 'pasta', 'spinach', 'coleslaw', 
     'jacket potato', 'baked beans'],
 }
 
+function randomIndex(num) {
+    return Math.floor(Math.random() * num);
+};
+
+let createdMeal = ['Your meal today is: ',];
+
+function selectMealComponents() {
+    for (const component in mealComponents) {
+        let index = randomIndex(mealComponents[component].length);
+        switch(component) {
+            case 'protein':
+                createdMeal.push(`Delicous ${mealComponents[component][index]}`);
+                break;
+            case 'vegetable':
+                createdMeal.push(`with ${mealComponents[component][index]}`);
+                break;
+            case 'side':
+                createdMeal.push(`and a side of ${mealComponents[component][index]}`);
+                break;
+            default:
+                return 'ERROR';
+        }
+        }
+    };
+
+selectMealComponents();
+console.log(createdMeal);
