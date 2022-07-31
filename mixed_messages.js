b9ca4bba2d6db0ab54eb1720301bf85d30519db4
JoelMitch <107200469+JoelMitch@users.noreply.github.com>
2022-07-31 11:37:13 +0100

    Initial Commit

---
+++ mixed_messages.js
@@ -0,0 +1,3 @@
+/* Idea: A random meal generator that will randomly select three components that make up a meal.
+The different components will consist of a type of protein, a side, and a type of vegetable. 
+These will then be logged in a format so the user can see the meal that has been chosen for them.*/
